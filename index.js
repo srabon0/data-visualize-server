1f3015e244e1e10540816213b38111d055c4d500
srabon0 <srabonemam4@gmail.com>
2022-06-17 20:26:57 +0600

    shi

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${"srabonBDale"}:${"mpIPu7DcBUcAdrHP"}@cluster0.qljfq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qljfq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
